ec11068c28a339428ca9d2479ab8c09eec4fb249
s-sujin-99 <t2684258@gmail.com>
2026-07-28 09:49:27 +0900

    [create] section01 Date 객체와 날짜

---
+++ section01/chapter17.js
@@ -0,0 +1,12 @@
+//1. Date 객체를 생성
+let date = new Date()
+console.log(date)
+
+//2. Date 객체 생성
+let dateate2 = new Date(2026,7-1,24,16,1,1);
+console.log(date2)
+
+//3. 타임스탬프 (1970,1,1,0,0,0 ~ 현재까지 ms 리턴) 1초 = 1000
+let date3 = new Date()
+const timeStamp = date3.getTime()
+console.log(timeStamp)
