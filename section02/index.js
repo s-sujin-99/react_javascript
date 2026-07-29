bd2ff1ea837284c0e1e87b1d96a550ec9b87c0bf
s-sujin-99 <t2684258@gmail.com>
2026-07-29 17:20:51 +0900

    [create] section02 외부모듈

--- section02/index.js
+++ section02/index.js
@@ -1,1 +1,17 @@
-console.log("안녕 node.js");
+//require(url) -> 외부 모듈 가져오기
+// const moduleDate = require('./src/math')
+// import add from './src/math.js'; //-> 객체로 받기
+// import * as moduleDate from './src/math.js';
+import multiply,{add, sub} from './src/math.js';
+import randomColor from 'randomcolor';
+
+//randomColor를 사용해 색상을 얻는다
+const color = randomColor();
+console.log(`randomColor() = ${color}`);
+
+/*
+// 외부 모듈에 있는 함수 가져와 사용
+console.log(add(1,2));
+console.log(sub(1,2));
+console.log(multiply(2,3));
+*/
---
+++ section02/src/math.js
@@ -0,0 +1,15 @@
+// math 모듈 생성
+export function add(a,b){
+    return a+b;
+}
+
+export function sub(a,b){
+    return a-b;
+}
+
+// 외부에 add,sub 함수 전송
+// export {add, sub};
+
+export default function multiply(a, b) {
+ return a * b;
+}
