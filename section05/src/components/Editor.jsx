19677a273fcf5d8c184da0038fccf98d33a8b0bf
s-sujin-99 <t2684258@gmail.com>
2026-08-03 16:12:16 +0900

    [create] section05 todo-list project 진행

---
+++ section05/src/components/Editor.jsx
@@ -0,0 +1,35 @@
+import { useState, useRef } from 'react';
+import '../Editor.css'
+const Editor = ({onCreate}) => {
+    const [content, setContent] = useState('');
+    const inputRef = useRef();
+    
+    const onChangeContent = (e) => {
+        setContent(e.target.value);
+    };
+
+    const onKeyDown =(e)=>{
+        if(e.keyCode === 13) onSubmit();
+    };
+
+    const onSubmit = () => {
+        if(content === ''){
+            inputRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        setContent('');
+    };
+ return (
+ <div className="Editor">
+    <input 
+    ref={inputRef}
+    value={content}
+    onChange={onChangeContent}
+    onKeyDown={onKeyDown}
+    placeholder="새로운 Todo..." />
+    <button onClick={onSubmit}>추가</button>
+ </div>
+ );
+};
+export default Editor;
